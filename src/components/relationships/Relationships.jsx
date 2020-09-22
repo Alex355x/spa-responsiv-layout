f6c55e1190f0bc1caadbe3e2da075d70654e3f12
Alexey <apill@ukr.net>
2020-09-22 20:21:40 +0300

    styles change

--- src/components/relationships/Relationships.jsx
+++ src/components/relationships/Relationships.jsx
@@ -12,6 +12,7 @@ const Relationships = () => {
           <h2 className='relation__h2'>My aim - is to be a cool frontend developer</h2>
           <p className='relation__p_part1'>Over the last year I got experience  and strong  knowledges of fundamentals and popular techniques of HTML5/CSS, modern JavaScript(ES6+), React, React-router, Redux, Node Modules, Webpack, VSCode, Github, understanding RESTfull APIs. For today I'm looking for new job opportunity. I have strong desire to grow and develop in Frontend development and engineering.  I have Intermediate English level - both - write and speak. Ready to discuss your proposition.
           </p>
+          
           <p className='relation__p_part2'>Other work experience: more 5 years as account manager in several companies (most of them were Startups) where I have deals with sales spare parts, different equipment, developing relationships with customers, customers support, helping to administrate websites..
           </p>
         <button className='relation__btn'><a className='relation__btn_a' href="#reg">Sing up now</a></button>
